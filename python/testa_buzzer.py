887a2c4ee719daa891e492573d900089ecd8a297
Eduardo Araujo <eduardo.araujo0@outlook.com>
2016-07-02 13:47:57 -0300

    Programa para testar o buzzer

---
+++ python/testa_buzzer.py
@@ -0,0 +1,21 @@
+#Programa: Testa o buzzer
+#Autor: Eduardo Araujo Dev
+
+import RPi.GPIO as GPIO
+import time
+
+tempo = 1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(22, GPIO.OUT)
+
+while True:
+	GPIO.output(22, 0)
+
+	time.sleep(tempo)
+
+	GPIO.output(22, 1)
+
+	time.sleep(tempo)
